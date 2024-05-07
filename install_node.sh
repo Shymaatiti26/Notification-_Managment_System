9db07805c00e32ebb036a034af8923e79bf28e83
Shymaatiti26 <100626034+Shymaatiti26@users.noreply.github.com>
2024-05-07 14:15:41 +0300

    Add files via upload

---
+++ install_node.sh
@@ -0,0 +1,5 @@
+git clone <https://github.com/Shymaatiti26/Notification-_Managment_System>
+
+cd server
+npm install
+npm start
---
+++ install_react.sh
@@ -0,0 +1,7 @@
+
+git clone <https://github.com/Shymaatiti26/Notification-_Managment_System>
+
+cd client
+npm install --force
+npm run dev
+
